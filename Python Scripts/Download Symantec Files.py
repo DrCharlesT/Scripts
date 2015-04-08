169e57f75f8d1166536d57923cbbeb2c85ecf1bb
DrCharlesT <charlesdardaman@Charless-MacBook-Pro.local>
2015-04-08 13:12:30 -0400

    Testing
    This is currently being tested. All of the functions are complete other
    than the create file function for output. Currently it is all going to
    the command line.

--- Python Scripts/Download Symantec Files.py
+++ Python Scripts/Download Symantec Files.py
@@ -40,6 +40,7 @@ def main():
 	#Puts the 3 links into a list to be easily used in a loop
 	downloads = [v5i32,v5i64,unix]
 
+	#Creates a list to be appended to
 	file_names = []
 
 	#Get the file name
@@ -49,8 +50,21 @@ def main():
 
 	#Calls the function to get the MD5s
 	MD5_hashes = get_MD5(file_names)
-	#Calls the function to download the files
-	#download_files(downloads)
+
+	#Calls the function to download the files and get their filenames
+	saved_file_names = download_files(downloads)
+
+	#Gets the file's hashes
+	file_MD5_hashes = get_file_MD5(saved_file_names)
+
+	#Compares the MD5 hashes
+	if compare_MD5(MD5_hashes,file_MD5_hashes):
+		for x in xrange(0, len(file_MD5_hashes)):
+			print MD5_hashes[x] + "   " + file_MD5_hashes[x]
+	else:
+		print "MD5 hashes failed"
+
+
 
 def get_MD5(file_names):
 	#Opens the website to the MD5-hash list
@@ -71,13 +85,42 @@ def get_MD5(file_names):
 			print line
 			unix_hash = line[:32]
 			print unix_hash
+	#Creates a list the hold the hashes
+	MD5_hashes = [v5i32_hash,v5i64_hash,unix_hash]
 
-return MD5_hashes = [v5i32_hash,v5i64_hash,unix_hash]
+	#Returns a list of hashes
+	return MD5_hashes	
 
+def get_file_MD5(saved_file_names):
+	file_MD5_hashes = []
+	for x in xrange(0,len(saved_file_names)):
+		#Opens the file and reads the data
+		with open(saved_file_names[x], 'rb') as fh:
+			m = hashlib.md5()
+			while True:
+				#Only reads a portion of the data at a time
+				data = fh.read(8192)
+				if not data:
+					break
+				m.update(data)
+			#Adds the MD5 to a list of MD5s
+			file_MD5_hashes.append(m.hexdigest())
+			print m.hexdigest()
+	#Returns the list of MD5 hashes
+	return file_MD5_hashes
 
+def compare_MD5(MD5_hashes,file_MD5_hashes):
+	#Compares the MD5 hashes, They will be in the same order
+	if MD5_hashes == file_MD5_hashes:
+		return True
+	else:
+		return False
+	print MD5_hashes
+	print file_MD5_hashes	
 
 
 def download_files(downloads):		
+	saved_file_names = []
 	for x in xrange(0,len(downloads)):	
 		#Gets the correct download link
 		url = downloads[x]
@@ -95,17 +138,24 @@ def download_files(downloads):
 		file_size_dl = 0
 		block_sz = 8192
 		while True:
-		    buffer = u.read(block_sz)
-		    if not buffer:
-		        break
+			buffer = u.read(block_sz)
+			if not buffer:
+				break
 
-		    file_size_dl += len(buffer)
-		    f.write(buffer)
-		    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-		    status = status + chr(8)*(len(status)+1)
-		    print status,
+			file_size_dl += len(buffer)
+			f.write(buffer)
+			status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+			status = status + chr(8)*(len(status)+1)
+			print status,
 
+		#Closes the file
 		f.close()
+
+		#Saves all of the file names, this is mainly used if the filename is changed
+		saved_file_names.append(file_name)
+	
+	#Returns the filenames so they can be verified
+	return saved_file_names
 
 #Starts the Program
 main()
