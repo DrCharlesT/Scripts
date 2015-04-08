ca4292f014e6616b7be6a0c2dd43730879a8b60d
DrCharlesT <charlesdardaman@Charless-MacBook-Pro.local>
2015-04-08 11:49:45 -0400

    MD5 Download
    Downloads the MD5 from Symantec. Will add verification soon.

--- Python Scripts/Download Symantec Files.py
+++ Python Scripts/Download Symantec Files.py
@@ -1,5 +1,6 @@
 import urllib2
 import re
+import hashlib
 
 v5i32 = ""
 v5i63 = ""
@@ -9,6 +10,7 @@ def main():
 	#Opens the website
 	website = urllib2.urlopen("http://www.symantec.com/security_response/definitions/download/detail.jsp?gid=sep")
 
+	#Gets the html of the website
 	html = website.read()
 
 	#Finds all of the HTTP links on the page
@@ -33,14 +35,47 @@ def main():
 			unix = list_of_links[x]
 			print list_of_links[x]
 		#print x
-		
 		x+=1	
 
 	#Puts the 3 links into a list to be easily used in a loop
 	downloads = [v5i32,v5i64,unix]
 
+	file_names = []
+
+	#Get the file name
+	for x in xrange(0, len(downloads)):
+		file_names.append(downloads[x].split('/')[-1])
+		print file_names[x]
+
+	#Calls the function to get the MD5s
+	MD5_hashes = get_MD5(file_names)
 	#Calls the function to download the files
 	#download_files(downloads)
+
+def get_MD5(file_names):
+	#Opens the website to the MD5-hash list
+	website = urllib2.urlopen("http://www.symantec.com/avcenter/download/md5-hash.txt")
+
+	#Runs through the website looking for the correct filename
+	for line in website:
+		#print line
+		if file_names[0] in line:
+			print line
+			v5i32_hash = line[:32]
+			print v5i32_hash
+		if file_names[1] in line:
+			print line
+			v5i64_hash = line[:32]
+			print v5i64_hash
+		if file_names[2] in line:
+			print line
+			unix_hash = line[:32]
+			print unix_hash
+
+return MD5_hashes = [v5i32_hash,v5i64_hash,unix_hash]
+
+
+
 
 def download_files(downloads):		
 	for x in xrange(0,len(downloads)):	
@@ -72,4 +107,5 @@ def download_files(downloads):
 
 		f.close()
 
+#Starts the Program
 main()
